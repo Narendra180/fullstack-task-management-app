0fe6b6980c46d3d0111baf070b16e64a4021e9d5
narendra180 <narendrakanigiri18@gmail.com>
2023-10-02 02:12:49 +0530

    made changes for production

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/users/userRoutes.js";
 import taskRouter from "./routes/tasks/taskRoutes.js";
+import path from 'path';
 
 await connectDB();
 
@@ -19,7 +20,14 @@ app.use(cookieParser());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
 
-app.get("/", (req,res) => res.send("Server is ready"))
+if(process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname,'frontend/dist')));
+  app.get("*", (req,res) => res.sendFile(path.resolve(__dirname,"frontend/dist/index.html")))
+} else {
+  app.get("/", (req,res) => res.send("Server is ready"));
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
